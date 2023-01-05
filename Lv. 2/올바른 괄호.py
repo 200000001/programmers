0322fb000d8013e2f98a385bc851e7df382ca9d0
200000001 <zxcvasdf1126@naver.com>
2023-01-05 18:04:40 +0900

    Create 올바른 괄호.py

---
+++ Lv. 2/올바른 괄호.py
@@ -0,0 +1,10 @@
+def solution(s):
+    a = 0
+    for i in s:
+        if a < 0:
+            break
+        a = a + 1 if i == "(" else a - 1
+    return a == 0
+    
+    
+#https://school.programmers.co.kr/learn/courses/30/lessons/12909
